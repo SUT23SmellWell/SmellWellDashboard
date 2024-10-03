9c94a7afbdc99f1e2d95bf2fb916f7bb00f2c282
Nanyria <145432308+Nanyria@users.noreply.github.com>
2024-10-03 11:28:03 +0200

    TestCommit

--- UI/SMFrontEndAng/src/app/app.component.spec.ts
+++ UI/SMFrontEndAng/src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'SWFrontEnd' title`, () => {
+  it(`should have the 'SMFrontEndAng' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('SWFrontEnd');
+    expect(app.title).toEqual('SMFrontEndAng');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SWFrontEnd');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, SMFrontEndAng');
   });
 });
