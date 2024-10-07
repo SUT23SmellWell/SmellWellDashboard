026081305826ee0800d2b08e5bd94f696c78bf85
Nanyria <145432308+Nanyria@users.noreply.github.com>
2024-10-07 10:18:28 +0200

    Testfrontend (statisk)

--- UI/SMFrontEndAng/src/app/app.component.ts
+++ UI/SMFrontEndAng/src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { KeywordSalesComponent } from './components/keyword-sales/keyword-sales.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, KeywordSalesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
