a543e76004f16a1876f7519ea8c1709274a12050
Jonathanjohansson24 <jonathan.nordin24@gmail.com>
2024-10-07 11:53:43 +0200

    Made the endpoints prettier also a new endpoint with sales & budget

--- GoogleAPI/Program.cs
+++ GoogleAPI/Program.cs
@@ -1,4 +1,6 @@
 
+using GoogleSheetsAPI.EndPoint;
+
 namespace GoogleAPI
 {
     public class Program
@@ -29,50 +31,10 @@ namespace GoogleAPI
 
 
             app.MapControllers();
-            app.MapGet("/KWRANKING", GetDataAsync);
+            app.UseCustomEndpoints();
             app.Run();
         }
 
-
-
-
-        private static async Task<IResult> GetDataAsync()
-        {
-            var filePath = "C:\\Users\\jonat\\Downloads\\kwresearchstest2.csv"; // Update the path to your CSV file
-            var csvData = await ReadCsvFileAsync(filePath);
-
-            if (csvData.Length < 7)
-            {
-                return Results.BadRequest("CSV file does not contain enough data.");
-            }
-
-            var selectedRows = ExtractSelectedRows(csvData);
-            return Results.Json(selectedRows);
-        }
-
-        private static async Task<string[]> ReadCsvFileAsync(string filePath)
-        {
-            return await File.ReadAllLinesAsync(filePath);
-        }
-
-        private static object ExtractSelectedRows(string[] csvData)
-        {
-            return csvData
-                .Skip(3) // Skip the first 3 rows to start at row 4
-                .Take(5) // Take 5 rows from row 7
-                         //5to 10
-                .Select(row => row.Split(','))
-                .Select(columns => new
-                {
-                    KEYWORD = columns[0],
-                    RANK = columns[1],
-                    CVR = columns[3],
-                    IMPRESSIONS = columns[4],
-                    CLICKS = columns[5],
-                    SPEND = columns[6],
-                    TOTALSALES = columns[7]
-                })
-                .OrderBy(row => row.RANK);
-        }
+    
     }
 }
