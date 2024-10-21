18017e8ada62c535d796082610e46c2807db8d38
Nanyria <145432308+Nanyria@users.noreply.github.com>
2024-10-21 10:50:19 +0200

    fixat så att den automatiskt känner av vilken startmånad man är på i 6 months

--- UI/SMFrontEndAng/src/app/components/SalesOverview/sales-overview/sales-overview.component.ts
+++ UI/SMFrontEndAng/src/app/components/SalesOverview/sales-overview/sales-overview.component.ts
@@ -12,26 +12,25 @@ import { CommonModule } from '@angular/common';
 export class SalesOverviewComponent {
   year: number = 2024;
   dropdownVisible: boolean = false;
-  startMonth: number = 0; // Default to the first month
+  startMonth: number = new Date().getMonth(); // Set the start month to the current month index
   months: string[] = [
     'Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
     'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'
   ];
+
   @ViewChild('salesChart') salesChart!: SalesChartComponent;
 
   show6Months() {
     const currentMonth = new Date().getMonth();
-    this.startMonth = Math.max(0, currentMonth - 5); // Calculate the start month for the latest 6 months
-    console.log('Showing 6 months'); // Add this line for debugging
+    this.startMonth = Math.max(0, currentMonth - 5);
     this.salesChart.displayMonths = 6;
     this.salesChart.startMonth = this.startMonth;
     this.salesChart.loadMonthlyData();
   }
 
   show12Months() {
-    console.log('Showing 12 months'); // Add this line for debugging
     this.salesChart.displayMonths = 12;
-    this.salesChart.startMonth = 0; // Reset start month to 0 for 12 months
+    this.salesChart.startMonth = 0;
     this.salesChart.loadMonthlyData();
   }
 
@@ -42,7 +41,7 @@ export class SalesOverviewComponent {
   updateStartMonth(event: Event) {
     const selectElement = event.target as HTMLSelectElement;
     this.startMonth = parseInt(selectElement.value, 10);
-    this.salesChart.startMonth = this.startMonth; // Update the start month in the sales chart
-    this.salesChart.updateChart(); // Update the chart immediately after selecting the start month
+    this.salesChart.startMonth = this.startMonth;
+    this.salesChart.updateChart();
   }
 }
