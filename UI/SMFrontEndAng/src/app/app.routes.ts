4d528079103a0020cdf66171cfd125a798c66e90
LudwigSvensson <ludwig.gifrod@hotmail.com>
2024-10-21 16:38:53 +0200

    ja

--- UI/SMFrontEndAng/src/app/app.routes.ts
+++ UI/SMFrontEndAng/src/app/app.routes.ts
@@ -1,7 +1,7 @@
-import { Routes } from '@angular/router';
-import { BuilderPage } from './components/builder-page.component'; // Ensure the correct path
+// import { Routes } from '@angular/router';
+// import { BuilderPage } from './components/builder-page.component'; // Ensure the correct path
 
-export const routes: Routes = [
-  { path: 'builder-demo', component: BuilderPage }, // Add the builder-demo route
-  // You can add other routes here as needed
-];
+// export const routes: Routes = [
+//   { path: 'builder-demo', component: BuilderPage }, // Add the builder-demo route
+//   // You can add other routes here as needed
+// ];
